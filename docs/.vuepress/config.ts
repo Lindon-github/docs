530d6f9f5af1130a0c142c8aba5167eacf5eb8c7
yangyaodong <5263647+guodong_ll@user.noreply.gitee.com>
2025-01-17 16:52:20 +0800

    🚀 部署东西

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/docs/',
   lang: 'zh-CN',
   title: 'Lidon',
   description: '',
